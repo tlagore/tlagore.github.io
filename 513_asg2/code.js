42580d49db754c234eb02dddec92af1335d85f5f
TyroneL <tyronelagore@gmail.com>
2017-02-01 14:56:55 -0700

    added a directory for 513 assignment 2

---
+++ 513_asg2/code.js
@@ -0,0 +1,15 @@
+function getStats(txt) {
+    // you need to write your own code here
+    return {
+        nChars: 123,
+        nWords: 22,
+        nLines: 10,
+        nNonEmptyLines: 22,
+        averageWordLength: 3.3,
+        maxLineLength: 33,
+        allPalindromes: ["123", "kayak", "mom"],
+        tenLongestWords: ["xxxxxxxxx", "123444444"],
+        tenMostFrequentWords: ["a", "this", "the"]
+    };
+
+}
