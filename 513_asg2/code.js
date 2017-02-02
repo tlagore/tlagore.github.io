c2f03652619af63680e5106b7d36392d95dcf1b0
tyrone.lagore <tyrone.lagore@zone47-wd.cs.ucalgary.ca>
2017-02-01 20:03:56 -0700

    implemented a lot of functions

--- 513_asg2/code.js
+++ 513_asg2/code.js
@@ -1,15 +1,109 @@
 function getStats(txt) {
     // you need to write your own code here
+    
+    var TextParse = {
+	utils:{
+	    numChars: function(text){
+		return text.length;
+	    },
+	    numWords: function(text){
+		let strs = text.split(/[^A-Za-z0-9]/);
+		let numWords = 0;
+		for(let value of strs){
+		    if(value !== ""){
+			numWords++;
+		    }
+		}
+		return numWords;
+	    },
+	    numLines: function(text){
+		return text.split('\n').length;
+	    },
+	    numNonEmptyLines: function(text){
+		let strs = text.split('\n');
+		let numEmpty = 0;
+		for(let value of strs){
+		    if(value !== ""){
+			numEmpty++;
+		    }
+		}
+
+		return numEmpty;
+	    },
+	    averageWordLength: function(text){
+		let strs = text.split(/[^A-Za-z0-9]/);
+		let avgLength = 0;
+		let numWords = 0;
+
+		for(let value of strs){
+		    if(value !== ""){
+			numWords++;
+			avgLength += value.length;
+		    }
+		}
+
+		return avgLength / numWords;
+	    },
+	    maxLineLength: function(text){
+		let strs = text.split("\n");
+		let maxLength = 0;
+
+		for(let value of strs){
+		    maxLength = value.length > maxLength ? value.length : maxLength;
+		}
+
+		return maxLength;
+	    },
+	    palindromes: function(text){
+		let strs = text.split(/[^A-Za-z0-9]/);
+		let palindromes = [];
+		let stackified = [];
+		let unstackified = [];
+
+		//let string = ['H','e','l','l','o'];
+		//stackified.push(string[0]);
+		//alert(stackified);
+		
+		for(let value of strs){
+		    if(value.length > 0){
+
+			for(let i = 0; i < value.length; i++){
+			    stackified.push(value[i]);
+			}
+
+			let length = stackified.length;
+			for(let j = 0; j < length; j++){
+			    unstackified[j] = stackified.pop();
+			}
+
+			if (value === unstackified.join('')){
+			    palindromes.push(value);
+			}
+		    }
+
+		    stackified.length = 0;
+		    unstackified.length = 0;
+		}
+		return palindromes;
+	    },
+	    tenLongestWords: function(text){
+		let strs = text.split(/[^A-Za-z0-9]/);
+		strs.sort(function(a,b){ return a.length > b.length ? -1 : 1 });
+		alert(strs);
+		return strs;
+	    }
+	}
+    };
+    
     return {
-        nChars: 123,
-        nWords: 22,
-        nLines: 10,
-        nNonEmptyLines: 22,
-        averageWordLength: 3.3,
-        maxLineLength: 33,
-        allPalindromes: ["123", "kayak", "mom"],
-        tenLongestWords: ["xxxxxxxxx", "123444444"],
+        nChars: TextParse.utils.numChars(txt),
+        nWords: TextParse.utils.numWords(txt),
+        nLines: TextParse.utils.numLines(txt),
+        nNonEmptyLines: TextParse.utils.numNonEmptyLines(txt),
+        averageWordLength: TextParse.utils.averageWordLength(txt),
+        maxLineLength: TextParse.utils.maxLineLength(txt),
+        allPalindromes: TextParse.utils.palindromes(txt),
+        tenLongestWords: TextParse.utils.tenLongestWords(txt),
         tenMostFrequentWords: ["a", "this", "the"]
     };
-
 }
